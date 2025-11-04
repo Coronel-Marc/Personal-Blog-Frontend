e0d5d72cd8729b6505faddc7d5d8c757d995200f
Marcos <jorgede1097@gmail.com>
2025-11-04 10:37:21 -0300

    change

--- src/pages/public/AboutPage.tsx
+++ src/pages/public/AboutPage.tsx
@@ -4,8 +4,7 @@ import { Link } from 'react-router-dom'
 
 export const AboutPage = () => {
   //TODO: Substituir pela minha foto
-  const profileImageUrl = `https://the-code-and-road-amzn-s3.s3.us-east-2.amazonaws.com/1762179211693_image.jpeg?X-Amz-Algorithm=AWS4-HMAC-SHA256&X-Amz-Content-Sha256=UNSIGNED-PAYLOAD&X-Amz-Credential=ASIASVFDWVSH3HLNSFZW%2F20251103%2Fus-east-2%2Fs3%2Faws4_request&X-Amz-Date=20251103T203644Z&X-Amz-Expires=300&X-Amz-Security-Token=IQoJb3JpZ2luX2VjEJ3%2F%2F%2F%2F%2F%2F%2F%2F%2F%2FwEaCXVzLWVhc3QtMiJGMEQCIFl7sY5dZOSLA3NpQBwzIQT82vj0uX%2BqCQB41W5rBPQjAiAFbcPEXzRz1wUJpW6JXRdPPuNER1m0fVbf8LhcbwZYayrbAghmEAAaDDE4Mjg3OTQzMTgyMyIMb0kCJZbmiMvWgS6pKrgC41sHGd1Fg6ONj9fUJpeRVCMB%2BCzvXAZQ04tOlk%2BBg0uHe0rw5TaRZAUUc2FBAcbVyf70OpEak3BfT5KgD7qASyZQCe8ZDtY4q7VHQNF%2FaqJ8rnPsbBqnU0Ki7Hx2pB8DNxz3Pe5jLLc39988uM2Yp4peJmpPiZ5%2Bo7HZhriOvtoUF5GLZn1EiWKaG9Gw6Mrs%2FBCJBE9PmTEor%2BNqN%2FLxIUxUeWebCYjSGa1Tz34SosQecwDbknU93cWGnoIm7kLq2FhJ7ITbcJ1iQFeLV7RuYrsp0%2Fz%2B4V4g5a8coJjJ6i7C3uTXnzLpClTTkwLoEQFRr2cbzGcepL%2FAuryrdsXD71Me6zMwK5IRQtXp%2BgkT3qQRj3PvC47dvvX7MZzPdfR%2FE%2BpiOEpfaZWd6LSEzN4gJlad%2Bu8EI6RGMI3losgGOq4C6g%2FYAYS1RuRTZZzLznb8Br9JYSKwe9AqKFC7I9f4%2FoYcCtz91F3ekOumB9%2BpjofiD%2BFSwPj6fjYgRbDTtrtUwFovHXF5o1djJjG9LJqspSvNjq%2B4aKVMjpWZffJwN67%2BiJVFrqcnMlmVN30W02QmZ8t%2FaUva6oQY%2Banui8J6SZByuCrtL6Qu3f0dDSqQFZBeBeXm6sGbRKR2Qn3wTIvlPNpHxrY3sjXuE4ZWoqfrFlaZxLLnYt905OVSOtuNAz8b47yJBXzdMyzR1PgKhoGT%2BLnNAMmr9kKdQjScLpLtybYFWJlUBasLJIwucb0luMV%2BCioi2I9B7ynYxjNuTr1sPAhiH%2B9woa3CEy86QECqBbkr6j221kmIGjqAnWQ9pFYavIuaS%2BbLMg%2F89QQGzJM%3D&X-Amz-Signature=a3c9f1d1fe0996d6ee775a58fd9fee909e0afbf9d569f41383c63578cb9e51c4&X-Amz-SignedHeaders=host&response-content-disposition=inline`
-
+  const profileImageUrl = ""
   const GITHUB_URL = 'https://github.com/Coronel-Marc'
   const LINKEDIN_URL = 'https://www.linkedin.com/in/g-marcos/'
   return (
